233241c8de5ad1227a5fc7e49c67a2c5f014b0d9
potrik98 <potrik98@gmail.com>
2018-09-18 14:42:02 +0200

    Add some unsigned

--- gloom/src/geometry.hpp
+++ gloom/src/geometry.hpp
@@ -12,13 +12,13 @@ namespace geometry {
     ) {
         const float z = 0.0f;
         const float scale_x = 3.0f / 4.0f; // Scale in x with the aspect ratio, because the coords are between -1 and 1 in both directions
-        const int vertex_count = corners + 1; // One for the center aswell
+        const unsigned int vertex_count = corners + 1; // One for the center aswell
         float vertices[3 * vertex_count];
         vertices[0] = center_x;
         vertices[1] = center_y;
         vertices[2] = z;
-        for (int i = 0; i < corners; ++i) {
-            const int index_base = 3 * (i + 1);
+        for (unsigned int i = 0; i < corners; ++i) {
+            const unsigned int index_base = 3 * (i + 1);
             const float angle = 2.0f * 3.14159265f * i / corners;
             const float x = center_x + radius * cosf(angle);
             const float y = center_y + radius * sinf(angle);
@@ -27,9 +27,9 @@ namespace geometry {
             vertices[index_base + 2] = z;
         }
 
-        const int index_count = 3 * corners;
+        const unsigned int index_count = 3 * corners;
         int indices[index_count];
-        for (int i = 0; i < corners; ++i) {
+        for (unsigned int i = 0; i < corners; ++i) {
             indices[3 * i + 0] = 0;
             indices[3 * i + 1] = 1 + i;
             indices[3 * i + 2] = 1 + (i + 1) % corners;
@@ -53,12 +53,12 @@ namespace geometry {
         const float z = 0.0f;
         const float scale_x = 3.0f / 4.0f; // Scale in x with the aspect ratio, because the coords are between -1 and 1 in both directions
         const float decay = stroke_width + space; // how much it spirals inwards per turn
-        const int vertices_per_loop = 2 * corners; // Inside and outside
-        const int vertex_count = vertices_per_loop * loops;
+        const unsigned int vertices_per_loop = 2 * corners; // Inside and outside
+        const unsigned int vertex_count = vertices_per_loop * loops;
         float vertices[3 * vertex_count];
-        for (int i = 0; i < loops; ++i) {
+        for (unsigned int i = 0; i < loops; ++i) {
             const float loop_decay = i * decay;
-            for (int j = 0; j < corners; ++j) {
+            for (unsigned int j = 0; j < corners; ++j) {
                 const int index_base = 3 * ((i * vertices_per_loop) + (2 * j));
                 const float corner_decay = j * decay / corners;
                 const float angle = 2.0f * 3.14159265f * j / corners;
@@ -77,10 +77,10 @@ namespace geometry {
 
         const int index_count = 3 * corners * 2 * loops;
         int indices[index_count];
-        int index = 0;
-        for (int i = 0; i < loops; ++i) {
-            const int end = i == loops - 1 ? corners - 1 : corners; // Skip the last edge due to overflow
-            for (int j = 0; j < end; ++j) {
+        unsigned int index = 0;
+        for (unsigned int i = 0; i < loops; ++i) {
+            unsigned const int end = i == loops - 1 ? corners - 1 : corners; // Skip the last edge due to overflow
+            for (unsigned int j = 0; j < end; ++j) {
                 const int inner_vertex = (i * vertices_per_loop) + (2 * j);
                 const int outer_vertex = (i * vertices_per_loop) + (2 * j) + 1;
                 const int next_inner_vertex = (i * vertices_per_loop) + (2 * ((j + 1)));
